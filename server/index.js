380d6fa38a4b11237bafcc1b927e4f515faa99d0
Sebastian Queirolo <sebaq1989@gmail.com>
2019-08-26 15:35:34 -0500

    prep project for hosting on digital ocean

--- server/index.js
+++ server/index.js
@@ -1,21 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const pc = require('./controllers/puzzle_controller');
-const uc = require('./controllers/user_controller')
+const pc = require("./controllers/puzzle_controller");
+const uc = require("./controllers/user_controller");
 
 app.use(express.json());
 
-app.get('/api/puzzles', pc.getPuzzles);
-app.get('/api/puzzles/:id', pc.getPuzzle);
-app.put('/api/puzzles/:id', pc.editPuzzle);
-app.post('/api/puzzles', pc.addPuzzle);
-app.delete('/api/puzzles/:id', pc.deletePuzzle);
+app.use(express.static(`${__dirname}/../build`));
 
-app.get('/api/user', uc.getPuzzles)
-app.get('/api/user/:category', uc.getPuzzlesByCategory)
-app.put('/api/user/:id', uc.editPuzzle)
-app.post('/api/user', uc.addPuzzle)
-app.delete('/api/user/:id', uc.deletePuzzle)
+app.get("/api/puzzles", pc.getPuzzles);
+app.get("/api/puzzles/:id", pc.getPuzzle);
+app.put("/api/puzzles/:id", pc.editPuzzle);
+app.post("/api/puzzles", pc.addPuzzle);
+app.delete("/api/puzzles/:id", pc.deletePuzzle);
+
+app.get("/api/user", uc.getPuzzles);
+app.get("/api/user/:category", uc.getPuzzlesByCategory);
+app.put("/api/user/:id", uc.editPuzzle);
+app.post("/api/user", uc.addPuzzle);
+app.delete("/api/user/:id", uc.deletePuzzle);
 
 const PORT = 7070;
-app.listen(PORT, () => console.log('Listening on port ' + PORT));
+app.listen(PORT, () => console.log("Listening on port " + PORT));
